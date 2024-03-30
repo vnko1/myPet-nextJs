a3b406e27b72b0f852915e829b1465b41e7eadf6
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-30 19:17:21 +0200

    ref: change types

--- src/lib/database/models/sponsor/sponsor.type.ts
+++ src/lib/database/models/sponsor/sponsor.type.ts
@@ -1,21 +1,4 @@
 import { Document } from "mongoose";
-
-export type WorkDays = {
-  isOpen: boolean;
-  from: string;
-  to: string;
-  _id: string | number;
-};
-
-export interface SponsorType {
-  title: string;
-  url: string;
-  addressUrl: string;
-  imageUrl: string;
-  address: string | null;
-  phone: string | null;
-  email: string | null;
-  workDays: WorkDays[] | null;
-}
+import { SponsorType } from "@/types";
 
 export interface ISponsor extends SponsorType, Document {}
