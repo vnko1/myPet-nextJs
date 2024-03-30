0fc6f16246b332b5812d5028afce50c6ab05c841
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-30 11:12:31 +0200

    save

--- src/app/friends/_components/sponsor/sponsor.tsx
+++ src/app/friends/_components/sponsor/sponsor.tsx
@@ -2,6 +2,12 @@ import { FC } from "react";
 import { SponsorProps } from "./sponsor.type";
 import styles from "./sponsor.module.scss";
 
-const Sponsor: FC<SponsorProps> = () => {};
+const Sponsor: FC<SponsorProps> = ({ title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
 
 export default Sponsor;
