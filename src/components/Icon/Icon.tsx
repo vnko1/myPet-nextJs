707183c1e99598e12ca2a39f738a4e14580bcd46
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-03-30 16:06:00 +0200

    sav

--- src/components/Icon/Icon.tsx
+++
@@ -1,7 +0,0 @@
-import IcoMoon, { IconProps } from "react-icomoon";
-
-import iconSet from "@/assets/icons/selection.json";
-import { FC } from "react";
-
-const Icon: FC<IconProps> = (props) => <IcoMoon iconSet={iconSet} {...props} />;
-export default Icon;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export { default as Logo } from "./logo/logo";
-export { default as Icon } from "./icon/icon";
 export { default as Animation } from "./animation/animation";
 export { default as Modal } from "./modal/modal";
 export { UIButton } from "./buttons";
 export { SearchField } from "./fields";
 export { default as Loader } from "./loader/loader";
+// export { default as Icon } from "./icon/icon";
