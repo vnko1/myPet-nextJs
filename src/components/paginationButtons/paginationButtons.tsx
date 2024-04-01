6860fd97093f605201732047485885a7e1253193
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-01 13:47:23 +0300

    save

--- src/components/paginationButtons/paginationButtons.tsx
+++ src/components/paginationButtons/paginationButtons.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
 // import Link from "next/link";
+import styles from "./paginationButtons.module.scss";
 
 import { PaginationButtonsProps } from "./paginationButtons.type";
 
 const PaginationButtons: FC<PaginationButtonsProps> = () => {
-  return <div>PaginationButtons</div>;
+  return <div className={styles["pagination"]}>PaginationButtons</div>;
 };
 
 export default PaginationButtons;
