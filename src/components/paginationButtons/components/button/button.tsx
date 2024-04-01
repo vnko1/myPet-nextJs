d758f6769c487493475a5cca8d996e3e92c6d584
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-01 18:02:10 +0300

    save

--- src/components/paginationButtons/components/button/button.tsx
+++ src/components/paginationButtons/components/button/button.tsx
@@ -24,7 +24,7 @@ const Button: FC<ButtonProps> = ({
   disabled,
 }) => {
   const onHandleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    onClick(event);
+    onClick && onClick(event);
   };
   const iconButtonClassNames = cn(
     styles["button"],
