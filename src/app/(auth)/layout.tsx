cadfdefc447c538dc717a2ba0fa654c18e80e785
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-04 08:03:08 +0300

    save

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,47 @@
+"use client";
 import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import styles from "./styles.module.scss";
 
-function Layou({ children }: { children: React.ReactNode }) {
+function Layout({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname();
+
+  const isRegister = pathName === "/register";
+  function getTitle() {
+    if (isRegister) return "Registration";
+    return "Login";
+  }
+
+  function getLinkText() {
+    if (isRegister) return "Login";
+    return "Register";
+  }
+
+  function getPath() {
+    if (isRegister) return "/login";
+    return "/register";
+  }
+
   return (
     <main>
       <section className="section">
         <div className="container">
-          <div className={styles["auth"]}>{children}</div>
+          <div className={styles["auth"]}>
+            <h1 className={styles["auth__title"]}>{getTitle()}</h1>
+            {children}
+            <p className={styles["auth__text"]}>
+              Already have an account?
+              <Link href={getPath()} className={styles["link"]}>
+                {" "}
+                {getLinkText()}
+              </Link>
+            </p>
+          </div>
         </div>
       </section>
     </main>
   );
 }
 
-export default Layou;
+export default Layout;
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,21 +1,8 @@
 import React from "react";
 import SignUp from "../_components/signUp/signUp";
-import styles from "../styles.module.scss";
-import Link from "next/link";
 
 function Register() {
-  return (
-    <>
-      <h1 className={styles["auth__title"]}>Registration</h1>
-      <SignUp />
-      <p className={styles["auth__text"]}>
-        Already have an account?
-        <Link href="/login" className={styles["link"]}>
-          Login
-        </Link>
-      </p>
-    </>
-  );
+  return <SignUp />;
 }
 
 export default Register;
