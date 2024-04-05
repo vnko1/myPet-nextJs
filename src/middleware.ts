0965bb6f05d532e270e3d0ac8ba924dcd5d7b9b6
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-05 09:16:25 +0300

    s

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,13 @@ import type { NextRequest } from "next/server";
 const isAuth = false;
 
 export default function middleware(request: NextRequest) {
-  if (!isAuth) return NextResponse.rewrite(new URL("/register", request.url));
+  if (!isAuth) return NextResponse.redirect(new URL("/login", request.url));
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/user",
+  matcher: [
+    "/((?!sponsors|friends|login|register|api|_next/static|_next/image|news|.webp|favicon.ico|.*\\.webp$|$).*)",
+  ],
 };
