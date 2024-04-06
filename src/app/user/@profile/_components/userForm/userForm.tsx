9da9229d4302c947c471c1cf086a3ac07d4ebcb0
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-06 22:19:06 +0300

    save

--- src/app/user/@profile/_components/userForm/userForm.tsx
+++ src/app/user/@profile/_components/userForm/userForm.tsx
@@ -1,11 +1,40 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { UserFormProps } from "./userForm.type";
+import Image from "next/image";
+import { UIButton } from "@/components";
+import { IconEnum } from "@/types";
 import styles from "./userForm.module.scss";
 
 function UserForm({ user }: UserFormProps) {
-  console.log(user);
-  return <div className={styles["form"]}>UserForm</div>;
+  const [isEditing, setIsEditing] = useState(false);
+  const [imageUrl] = useState(user.avatarUrl);
+
+  // console.log(user);
+  return (
+    <form className={styles["form"]}>
+      <div className={styles["cross-btn-wrapper"]}>
+        <UIButton
+          variant="text"
+          color="accent"
+          iconSize={24}
+          onClick={() => {
+            setIsEditing(!isEditing);
+          }}
+          icon={!isEditing ? IconEnum.EDIT : IconEnum.CROSS}
+        />
+      </div>
+      <div>
+        <Image
+          width={182}
+          height={182}
+          src={imageUrl}
+          alt="user avatar"
+          className={styles["image"]}
+        />
+      </div>
+    </form>
+  );
 }
 
 export default UserForm;
