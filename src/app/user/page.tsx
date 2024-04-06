527bc58804913369bb1f6b287a6350b0193a2c3f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-06 10:03:26 +0300

    save

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { isAuth } from "@/lib/database";
 
-function User() {
-  return (
-    <section className="section">
-      <div className="container">User</div>
-    </section>
-  );
+async function User() {
+  const user = await isAuth();
+
+  if (user)
+    return (
+      <section className="section">
+        <div className="container">User</div>
+      </section>
+    );
+
+  return null;
 }
 
 export default User;
