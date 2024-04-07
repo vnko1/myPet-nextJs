f3df5c638c6966658d7f0b4fa00920e1e208677e
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-07 10:00:41 +0300

    save

--- src/lib/actions/user/index.ts
+++ src/lib/actions/user/index.ts
@@ -22,7 +22,7 @@ export async function register(formData: FormData) {
   } catch (error) {
     if (error instanceof Error) return errorResponse(error.message, error.name);
   }
-  revalidatePath(LinksEnum.HOME);
+
   redirect(LinksEnum.LOGIN);
 }
 
@@ -47,7 +47,7 @@ export async function login(formData: FormData) {
   } catch (error) {
     if (error instanceof Error) return errorResponse(error.message, error.name);
   }
-  revalidatePath(LinksEnum.HOME);
+
   redirect(LinksEnum.USER);
 }
 
