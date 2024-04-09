caf04a2982a281992f80b1a90cb78323b40a2797
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-09 22:49:24 +0300

    save

--- src/app/user/add-pet/details/page.tsx
+++ src/app/user/add-pet/details/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React from "react";
+import { useWatch } from "react-hook-form";
 
 function Details() {
+  const watch = useWatch();
+  console.log(watch);
   return <div>Details</div>;
 }
 
