1f3f0ab30d676b134649859fcaa36a6dcb69dce7
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-10 21:29:27 +0300

    feat:add error handler

--- src/app/user/add-pet/api/route.ts
+++ src/app/user/add-pet/api/route.ts
@@ -1,7 +1,13 @@
+import { errorResponse } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const res = await request.json();
-  console.log("🚀 ~ POST ~ request:", res);
-  return NextResponse.json({ status: 204 });
+  try {
+    const res = await request.json();
+    console.log("🚀 ~ POST ~ request:", res);
+    return NextResponse.json({ status: 204 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json(errorResponse(error.message));
+  }
 }
