bb79472d49259d6f110b26c0777d0d90110c69cf
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-10 12:08:51 +0300

    save

--- src/app/user/add-pet/_utils/types/index.ts
+++ src/app/user/add-pet/_utils/types/index.ts
@@ -1,12 +1,4 @@
-export type FormValues = {
-  category: string;
-  title?: string;
-  name: string;
-  date: string;
-  type: string;
-  sex: string;
-  imageUrl?: string;
-  location?: string;
-  price?: string;
-  comment?: string;
-};
+import * as z from "zod";
+import { petsSchema } from "@/schema";
+
+export type FormValues = z.infer<typeof petsSchema>;
