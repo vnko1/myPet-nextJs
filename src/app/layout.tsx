d85d0414e0144e49eadc07dab956970253819e6f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-11 20:55:42 +0300

    save

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,35 @@
 import type { Metadata } from "next";
+import { cookies } from "next/headers";
 
 import { manrope, inter, poppins } from "@/fonts";
 import { Header } from "@/app/_components";
+
+import { authenticate } from "@/auth";
 import "../styles/globals.scss";
 
-import { currentUser } from "@/lib/database";
 // eslint-disable-next-line react-refresh/only-export-components
 export const metadata: Metadata = {
   title: "My Pet App",
   description: "My Pet app - applications about pets",
 };
 
-export const revalidate = 1800;
+// export const revalidate = 1800;
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const user = await currentUser();
+  const token = cookies().get("token");
+
+  const user = token && (await authenticate(token.name, token.value));
 
   return (
     <html lang="en">
       <body
         className={`${manrope.variable} ${inter.variable} ${poppins.variable}`}
       >
-        <Header user={JSON.parse(JSON.stringify(user))} />
+        <Header user={JSON.parse(JSON.stringify(user || ""))} />
         {children}
       </body>
     </html>
