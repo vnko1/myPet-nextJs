de50166d3ddaddda5d0ad65452191cf3c54fa399
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-12 13:17:13 +0300

    save

--- src/app/notices/_components/filters/filters.tsx
+++ src/app/notices/_components/filters/filters.tsx
@@ -4,16 +4,19 @@ import React, { FC, useState } from "react";
 
 import { useGetScreenSize } from "@/hooks";
 import { Icon } from "@/components";
-import { IconEnum } from "@/types";
+import { IconEnum, LinksEnum } from "@/types";
 import { FilterProps } from "./filters.type";
 import { AuthModal } from "./components";
 import styles from "./filters.module.scss";
+import { useRouter } from "next/navigation";
 
-const Filters: FC<FilterProps> = () => {
+const Filters: FC<FilterProps> = ({ user }) => {
+  const { push } = useRouter();
   const [screenSize] = useGetScreenSize();
   const [isActive, setIsActive] = useState(false);
 
   const onHandleNavClick = () => {
+    if (user) return push(LinksEnum.ADD_PET_CATEGORY);
     setIsActive(true);
   };
 
