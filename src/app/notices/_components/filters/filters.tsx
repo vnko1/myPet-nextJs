184b9f01f48eeb88600d17f8d4c26c45ef5002bd
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-12 07:45:43 +0300

    add filters

--- src/app/notices/_components/filters/filters.tsx
+++ src/app/notices/_components/filters/filters.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import React, { FC } from "react";
 
 import { FilterProps } from "./filters.type";
 import styles from "./filters.module.scss";
+import { Icon } from "@/components";
+import { IconEnum } from "@/types";
 
 const Filters: FC<FilterProps> = () => {
-  return <div className={styles["filters"]}>Filters</div>;
+  return (
+    <div className={styles["filters"]}>
+      <button className={`${styles["button"]} ${styles["filter"]}`}>
+        <Icon icon={IconEnum.FILTERS} size={24} />
+      </button>
+      <button className={`${styles["button"]} ${styles["nav"]}`}></button>
+    </div>
+  );
 };
 
 export default Filters;
