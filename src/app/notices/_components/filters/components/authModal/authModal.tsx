1eaf36ffc239db0f8b92c86086cb3f03f79ecaf0
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-12 12:16:15 +0300

    save

---
+++ src/app/notices/_components/filters/components/authModal/authModal.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import { AuthModalProps } from "./authModal.type";
+import styles from "./authModal.module.scss";
+
+const AuthModal: FC<AuthModalProps> = () => {
+  return <div className={styles[""]}>AuthModal</div>;
+};
+
+export default AuthModal;
---
+++ src/app/notices/_components/filters/components/authModal/authModal.type.ts
@@ -0,0 +1,1 @@
+export type AuthModalProps = { classNames?: string };
