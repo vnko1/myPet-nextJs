e3fba05de5d1534dcf04e02a0043e66fa141d862
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-12 17:13:54 +0300

    style: add filter styles

--- src/app/notices/_components/filters/components/filterPopup/filterPopup.tsx
+++ src/app/notices/_components/filters/components/filterPopup/filterPopup.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import cn from "classnames";
 
 import { useOutsideEventHandler } from "@/hooks";
 import { FilterPopupProps } from "./filterPopup.type";
 import styles from "./filterPopup.module.scss";
+import { Icon } from "@/components";
+import { IconEnum } from "@/types";
 
 const FilterPopup: FC<FilterPopupProps> = ({
   active,
@@ -14,6 +16,9 @@ const FilterPopup: FC<FilterPopupProps> = ({
 }) => {
   const nodeRef = useRef(null);
 
+  const [genderIsActive, setGenderIsActive] = useState(false);
+  const [ageIsActive, setAgeIsActive] = useState(false);
+
   useOutsideEventHandler(nodeRef, eventHandler);
 
   useEffect(() => {
@@ -23,18 +28,48 @@ const FilterPopup: FC<FilterPopupProps> = ({
   }, [active, setIsVisible]);
 
   const popupClassNames = cn(
-    styles["filter"],
+    styles["filters"],
     {
       [styles["active"]]: isVisible,
     },
     classNames
   );
 
+  const ageButtonClassNames = cn(
+    styles["filter__button"],
+    styles["age__button"],
+    { [styles["active"]]: ageIsActive }
+  );
+
+  const genderButtonClassNames = cn(
+    styles["filter__button"],
+    styles["gender__button"],
+    { [styles["active"]]: genderIsActive }
+  );
+
   if (!active) return null;
 
   return (
     <div ref={nodeRef} className={popupClassNames}>
-      FilterPopup
+      <h2>Filters</h2>
+      <div className={`${styles["filter"]} ${styles["age"]}`}>
+        <button
+          className={ageButtonClassNames}
+          onClick={() => setAgeIsActive(!ageIsActive)}
+        >
+          <Icon icon={IconEnum.CHEVRON} size={24} />
+          By age
+        </button>
+      </div>
+      <div className={`${styles["filter"]} ${styles["gender"]}`}>
+        <button
+          className={genderButtonClassNames}
+          onClick={() => setGenderIsActive(!genderIsActive)}
+        >
+          <Icon icon={IconEnum.CHEVRON} size={24} />
+          By gender
+        </button>
+      </div>
     </div>
   );
 };
