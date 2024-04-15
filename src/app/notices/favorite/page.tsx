c044d346381d034f4ba9f59cc5d79ee43b8546c3
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-15 07:30:12 +0300

    save

--- src/app/notices/favorite/page.tsx
+++ src/app/notices/favorite/page.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { headers } from "next/headers";
+import { ConstantsEnum, NOTICES_LIMIT, NoticeQueryParams } from "@/types";
+import { JSONParser } from "@/utils";
+import { getNotices, getNoticesPages } from "@/lib/database";
+import { Pagination, Notices } from "@/app/_components";
+import layoutStyles from "@/app/notices/notices.module.scss";
+
+type PageProps = { searchParams: NoticeQueryParams };
+
 export const dynamic = "force-dynamic";
 export const revalidate = 300;
-function FavoritePage() {
-  return <div>FavoritePage</div>;
+
+async function FavoritePage({ searchParams }: PageProps) {
+  const query: NoticeQueryParams = { ...searchParams, category: "favorites" };
+  const totals = await getNoticesPages(query);
+  const data = await getNotices(query);
+
+  const notices = JSONParser(data);
+
+  const userId = headers().get(ConstantsEnum.USER_ID);
+  return (
+    <>
+      <div className={layoutStyles["content-wrapper"]}>
+        <Notices notices={notices || []} userId={userId} />
+      </div>
+      <Pagination totals={totals || 0} limit={NOTICES_LIMIT} />
+    </>
+  );
 }
 
 export default FavoritePage;
