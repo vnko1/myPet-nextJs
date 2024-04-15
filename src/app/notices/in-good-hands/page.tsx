6027712302336d97d4e35d5fe58829696be75ea6
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-15 07:18:06 +0300

    save

--- src/app/notices/in-good-hands/page.tsx
+++ src/app/notices/in-good-hands/page.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-export const dynamic = "force-dynamic";
+import { headers } from "next/headers";
+import { ConstantsEnum, NOTICES_LIMIT, NoticeQueryParams } from "@/types";
+import { JSONParser } from "@/utils";
+import { getNotices, getNoticesPages } from "@/lib/database";
+import { Pagination, Notices } from "@/app/_components";
+import layoutStyles from "@/app/notices/notices.module.scss";
 
+type PageProps = { searchParams: NoticeQueryParams };
+
+export const dynamic = "force-dynamic";
 export const revalidate = 300;
-function InGoodHandsPage() {
-  return <div>InGoodHandsPage</div>;
+
+async function InGoodHandsPage({ searchParams }: PageProps) {
+  const query: NoticeQueryParams = {
+    ...searchParams,
+    category: "in-good-hands",
+  };
+  const totals = await getNoticesPages(query);
+  const data = await getNotices(query);
+  const notices = JSONParser(data);
+  const userId = headers().get(ConstantsEnum.USER_ID);
+
+  return (
+    <>
+      <div className={layoutStyles["content-wrapper"]}>
+        <Notices notices={notices || []} userId={userId} />
+      </div>
+      <Pagination totals={totals || 0} limit={NOTICES_LIMIT} />
+    </>
+  );
 }
 
 export default InGoodHandsPage;
